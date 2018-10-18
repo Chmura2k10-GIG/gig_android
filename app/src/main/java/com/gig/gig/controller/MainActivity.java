2f4d4604a97bcea46d97accd41fa2ad7bccdbc98
berendhard <berendhard@gmail.com>
2018-10-18 22:14:23 +0200

    add Spring dependency

--- app/src/main/java/com/gig/gig/controller/MainActivity.java
+++ app/src/main/java/com/gig/gig/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.gig.gig;
+package com.gig.gig.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.gig.gig.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/test/java/com/gig/gig/ExampleUnitTest.java
+++ app/src/test/java/com/gig/gig/ExampleUnitTest.java
@@ -1,6 +1,8 @@
 package com.gig.gig;
 
 import org.junit.Test;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.*;
 
@@ -14,4 +16,19 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void springTest(){
+        String url = "https://ajax.googleapis.com/ajax/" +
+                "services/search/web?v=1.0&q={query}";
+        // Create a new RestTemplate instance
+                RestTemplate restTemplate = new RestTemplate();
+
+        // Add the String message converter
+                restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+
+        // Make the HTTP GET request, marshaling the response to a String
+                String result = restTemplate.getForObject(url, String.class, "Android");
+    }
 }
+
